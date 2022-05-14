2a9768c1fcca80aaae164db91a793d71fc5dc517
Mutyala Harshith <105526704+Mega-Hero@users.noreply.github.com>
2022-05-14 12:52:21 +0530

    Create text.py

---
+++ renamer/tools/text.py
@@ -0,0 +1,50 @@
+from pyrogram.emoji import *
+
+class TEXT:
+    DOWNLOAD_START = f"Downloading Starts soon {SLEEPING_FACE}"
+    UPLOAD_START = f"Upload Starting Soon {SLEEPING_FACE}"
+    UPLOAD_SUCESS = f"Thanks for using [me](https://t.me/MutyalaHarshith)"
+    BANNED_USER_TEXT = f"Hey bro, you are **banned** from using me {FACE_WITH_TEARS_OF_JOY}."
+    NOT_LOGGED_TEXT = f"This bot was only for private use {LOCKED_WITH_KEY}. If you want to use this bot you need to send me correct password in the format `/login bot_password`"
+    SAVED_CUSTOM_THUMBNAIL = f"Thumbnail Saved Permanently {NOTEBOOK_WITH_DECORATIVE_COVER}"
+    DELETED_CUSTOM_THUMBNAIL = f"Thumbnail Deleted Successfully {CHECK_MARK_BUTTON}"
+    NO_CUSTOM_THUMB_NAIL_FOUND = f"𝖭𝗈 𝗍𝗁𝗎𝗆𝖻𝗇𝖺𝗂𝗅 𝖥𝗈𝗎𝗇𝖽 {THUMBS_DOWN_LIGHT_SKIN_TONE}"
+    THUMBNAIL_CAPTION = f"{BACKHAND_INDEX_POINTING_UP_LIGHT_SKIN_TONE} Your Permanent thumbnail"
+
+
+    ABOUT = """**𝖣𝖾𝗍𝖺𝗂𝗅𝗌 fRom @Telugu_robots :**
+** My Name:** {bot_name}
+    
+** Language:** [Python 3](https://www.python.org/)
+** FrameWork:** [Pyrogram](https://github.com/pyrogram/pyrogram)
+** Developer:** {bot_owner}
+** Channel:** [Harshith](https://t.me/MutyalaHarshith)
+** Group:** [Harshith Bots Support](https://t.me/Harshith_bots)
+** Follow:** [YouTube](https://youtube.com/channel/UCLUGJNpUkBufc19xHuiaC_Q)
+"""
+
+    HELP_USER = """**Follow Below Steps of @Telugu_Robots:**
+   
+☞︎︎︎ Use /mode command to change upload mode.
+☞︎︎︎ Send a photo to set as permanent thumbnail.
+☞︎︎︎ Now send me the Telegram file you want to rename.
+☞︎︎︎ Send the new name when bot ask.
+Have any issues contact @Harshith_Bots
+"""
+
+    START_TEXT = """Hi {user_mention},
+I am a telegram renamer with permanent thumbnail.
+𝖥𝗈𝗋 𝖬𝗈𝗋𝖾 𝖣𝖾𝗍𝖺𝗂𝗅𝗌 𝖲𝖾𝖾 𝖧𝖾𝗅𝗉 @MutyalaHarshith.
+**Maintained By:** {bot_owner}
+"""
+
+
+    DONATE_USER = """**__Thanks for showing interest in donation.__**
+ 
+I don't want donation
+ 
+__--Payment Methods:--__
+ 
+I don't want donation 😄😄
+ 
+** Please add users in @Harshith_Bots"""
